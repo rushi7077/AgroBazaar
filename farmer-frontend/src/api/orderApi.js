829b2ada107b54f5e0922fda61bd32b04444e308
Rushi Baravkar <rushibaravkar1012@gmail.com>
2026-02-14 22:45:08 +0530

    updated

--- farmer-frontend/src/api/orderApi.js
+++ farmer-frontend/src/api/orderApi.js
@@ -14,6 +14,11 @@ export const getFarmerOrdersApi = async () => {
   return res.data;
 };
 
+/* ADMIN */
+export const getAllOrdersApi = async () => {
+  const res = await api.get("/api/orders");
+  return res.data; // ⭐ VERY IMPORTANT
+};
 
 /* SELLER ACTIONS */
 export const decideItemApi = (itemId, decision) =>
